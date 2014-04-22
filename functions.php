09f7cb36a413d0eb0cb5fa19c3acb9ee4a94b49b
frank <112488740@qq.com>
2014-04-22 10:21:17 +0800

    functions.php

---
+++ functions.php
@@ -0,0 +1,210 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2012 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+/**
+ * 浏览器友好的变量输出
+ * @param mixed $var 变量
+ * @param boolean $echo 是否输出 默认为True 如果为false 则返回输出字符串
+ * @param string $label 标签 默认为空
+ * @param boolean $strict 是否严谨 默认为true
+ * @return void|string
+ */
+function dump($var, $echo=true, $label=null, $strict=true) {
+    $label = ($label === null) ? '' : rtrim($label) . ' ';
+    if (!$strict) {
+        if (ini_get('html_errors')) {
+            $output = print_r($var, true);
+            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+        } else {
+            $output = $label . print_r($var, true);
+        }
+    } else {
+        ob_start();
+        var_dump($var);
+        $output = ob_get_clean();
+        if (!extension_loaded('xdebug')) {
+            $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
+            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+        }
+    }
+    if ($echo) {
+        echo($output);
+        return null;
+    }else
+        return $output;
+}
+
+
+/**
+ * URL重定向
+ * @param string $url 重定向的URL地址
+ * @param integer $time 重定向的等待时间（秒）
+ * @param string $msg 重定向前的提示信息
+ * @return void
+ */
+function redirect($url, $time=0, $msg='') {
+    //多行URL地址支持
+    $url        = str_replace(array("\n", "\r"), '', $url);
+    if (empty($msg))
+        $msg    = "系统将在{$time}秒之后自动跳转到{$url}！";
+    if (!headers_sent()) {
+        // redirect
+        if (0 === $time) {
+            header('Location: ' . $url);
+        } else {
+            header("refresh:{$time};url={$url}");
+            echo($msg);
+        }
+        exit();
+    } else {
+        $str    = "<meta http-equiv='Refresh' content='{$time};URL={$url}'>";
+        if ($time != 0)
+            $str .= $msg;
+        exit($str);
+    }
+}
+
+/**
+ * 快速文件数据读取和保存 针对简单类型数据 字符串、数组
+ * @param string $name 缓存名称
+ * @param mixed $value 缓存值
+ * @param string $path 缓存路径
+ * @return mixed
+ */
+function F($name, $value='', $path=DATA_PATH) {
+    static $_cache  = array();
+    $filename       = $path . $name . '.php';
+    if ('' !== $value) {
+        if (is_null($value)) {
+            // 删除缓存
+            return false !== strpos($name,'*')?array_map("unlink", glob($filename)):unlink($filename);
+        } else {
+            // 缓存数据
+            $dir            =   dirname($filename);
+            // 目录不存在则创建
+            if (!is_dir($dir))
+                mkdir($dir,0755,true);
+            $_cache[$name]  =   $value;
+            return file_put_contents($filename, strip_whitespace("<?php\treturn " . var_export($value, true) . ";?>"));
+        }
+    }
+    if (isset($_cache[$name]))
+        return $_cache[$name];
+    // 获取缓存数据
+    if (is_file($filename)) {
+        $value          =   include $filename;
+        $_cache[$name]  =   $value;
+    } else {
+        $value          =   false;
+    }
+    return $value;
+}
+
+/**
+ * 根据PHP各种类型变量生成唯一标识号
+ * @param mixed $mix 变量
+ * @return string
+ */
+function to_guid_string($mix) {
+    if (is_object($mix) && function_exists('spl_object_hash')) {
+        return spl_object_hash($mix);
+    } elseif (is_resource($mix)) {
+        $mix = get_resource_type($mix) . strval($mix);
+    } else {
+        $mix = serialize($mix);
+    }
+    return md5($mix);
+}
+
+/**
+ * XML编码
+ * @param mixed $data 数据
+ * @param string $encoding 数据编码
+ * @param string $root 根节点名
+ * @return string
+ */
+function xml_encode($data, $encoding='utf-8', $root='think') {
+    $xml    = '<?xml version="1.0" encoding="' . $encoding . '"?>';
+    $xml   .= '<' . $root . '>';
+    $xml   .= data_to_xml($data);
+    $xml   .= '</' . $root . '>';
+    return $xml;
+}
+
+/**
+ * 数据XML编码
+ * @param mixed $data 数据
+ * @return string
+ */
+function data_to_xml($data) {
+    $xml = '';
+    foreach ($data as $key => $val) {
+        is_numeric($key) && $key = "item id=\"$key\"";
+        $xml    .=  "<$key>";
+        $xml    .=  ( is_array($val) || is_object($val)) ? data_to_xml($val) : $val;
+        list($key, ) = explode(' ', $key);
+        $xml    .=  "</$key>";
+    }
+    return $xml;
+}
+
+
+/**
+ * 获取客户端IP地址
+ * @param integer $type 返回类型 0 返回IP地址 1 返回IPV4地址数字
+ * @return mixed
+ */
+function get_client_ip($type = 0) {
+	$type       =  $type ? 1 : 0;
+    static $ip  =   NULL;
+    if ($ip !== NULL) return $ip[$type];
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        $pos    =   array_search('unknown',$arr);
+        if(false !== $pos) unset($arr[$pos]);
+        $ip     =   trim($arr[0]);
+    }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip     =   $_SERVER['HTTP_CLIENT_IP'];
+    }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+        $ip     =   $_SERVER['REMOTE_ADDR'];
+    }
+    // IP地址合法验证
+    $long = sprintf("%u",ip2long($ip));
+    $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+    return $ip[$type];
+}
+
+/**
+ * 发送HTTP状态
+ * @param integer $code 状态码
+ * @return void
+ */
+function send_http_status($code) {
+    static $_status = array(
+        // Success 2xx
+        200 => 'OK',
+        // Redirection 3xx
+        301 => 'Moved Permanently',
+        302 => 'Moved Temporarily ',  // 1.1
+        // Client Error 4xx
+        400 => 'Bad Request',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        // Server Error 5xx
+        500 => 'Internal Server Error',
+        503 => 'Service Unavailable',
+    );
+    if(isset($_status[$code])) {
+        header('HTTP/1.1 '.$code.' '.$_status[$code]);
+        // 确保FastCGI模式下正常
+        header('Status:'.$code.' '.$_status[$code]);
+    }
+}
